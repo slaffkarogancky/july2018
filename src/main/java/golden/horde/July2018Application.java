b60d15068e234177d0ffaae72da5d3321e440e55
Khan <some@mail>
2018-07-11 11:13:44 +0300

    First commit

---
+++ src/main/java/golden/horde/July2018Application.java
@@ -0,0 +1,12 @@
+package golden.horde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class July2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(July2018Application.class, args);
+	}
+}
